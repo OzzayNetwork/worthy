1b2cff0cb3c939c25024e1db9b179d065f204952
ozzaynetworks <ozzaynetworks@gmail.com>
2022-06-16 12:47:06 +0300

    kelvin: the owl corrected

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,7 +17,8 @@ $(".owl-carousel-product").owlCarousel({
         414: { items: 1, },
         698: { items: 2, },
         768: { items: 3, },
-        1200: { items: 4, }
+        1200: { items: 4, },
+        1550: { items: 5, }
     }
 });
 
